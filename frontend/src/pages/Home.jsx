8628eb0f8769fc0952b898c02da0d6124fa23a05
Donald Bassa <donaldbassa@192.168.1.6>
2023-07-06 11:49:24 +0000

    homepage terminée

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,10 +11,14 @@ import featureImg from "../assets/images/feature-img.png";
 import videoIcon from "../assets/images/video-icon.png";
 import avatarIcon from "../assets/images/avatar-icon.png";
 
+import faqImg from "../assets/images/faq-img.png";
+
 import { Link } from "react-router-dom";
 import About from "../components/About/About";
 import ServiceList from "../components/Services/ServiceList";
 import DoctorList from "../components/Doctors/DoctorList";
+import FaqList from "../components/Faq/FaqList";
+import Testimonial from "../components/Testimonial/Testimonial";
 
 const Home = () => {
   return (
@@ -275,6 +279,42 @@ const Home = () => {
         </div>
       </section>
       {/** =========== End Our great Doctor ============ */}
+
+      {/** =========== FAQ section ============ */}
+      <section>
+        <div className="container">
+          <div className="flex justify-between gap-[50px] lg:gap-0">
+            <div className="w-1/2 hidden md:block">
+              <img src={faqImg} alt="faq" />
+            </div>
+
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">
+                Most questions by our beloved patients
+              </h2>
+
+              <FaqList />
+            </div>
+          </div>
+        </div>
+      </section>
+      {/** =========== End FAQ section ============ */}
+
+      {/** =========== Testimonials section ============ */}
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center"> What our patient say</h2>
+            <p className="text__para text-center">
+              {" "}
+              World-class care for everyone. our health System offers unmatched,
+              expert health care
+            </p>
+          </div>
+          <Testimonial />
+        </div>
+      </section>
+      {/** =========== End Testimonials section ============ */}
     </>
   );
 };
